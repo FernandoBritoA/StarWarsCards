7c43364876303fc766753bee421492fd0a894017
FernandoBritoA <Fernando_BritoA@hotmail.com>
2020-03-25 17:35:26 -0500

    Updates

---
+++ precache-manifest.222a7090de6df7009e97c3fc7784f11c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a781e72ecdde22da02d1766dd5eac0",
+    "url": "/StarWarsCards/index.html"
+  },
+  {
+    "revision": "d6e1ee57d0dc516d71d5",
+    "url": "/StarWarsCards/static/css/main.b9e7a83e.chunk.css"
+  },
+  {
+    "revision": "10e7da7110e85e197d47",
+    "url": "/StarWarsCards/static/js/2.b0929f4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/StarWarsCards/static/js/2.b0929f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e1ee57d0dc516d71d5",
+    "url": "/StarWarsCards/static/js/main.95fedb54.chunk.js"
+  },
+  {
+    "revision": "3056127cb1901f231b97",
+    "url": "/StarWarsCards/static/js/runtime-main.697bccb9.js"
+  },
+  {
+    "revision": "701046a5c3b814e9ceeb69dabcfa4a58",
+    "url": "/StarWarsCards/static/media/Death Star.701046a5.woff"
+  },
+  {
+    "revision": "01d85fb120cf296268a13d7467cbc39c",
+    "url": "/StarWarsCards/static/media/boba.01d85fb1.jpg"
+  },
+  {
+    "revision": "21e4022cfea3672cbb9ff60e50d2cf83",
+    "url": "/StarWarsCards/static/media/c3po.21e4022c.png"
+  },
+  {
+    "revision": "16895859f9d4c46c59d0adc3b52afc55",
+    "url": "/StarWarsCards/static/media/chewbacca.16895859.png"
+  },
+  {
+    "revision": "392b78fb33222055fe8ff68c75cd50a9",
+    "url": "/StarWarsCards/static/media/darthSidious.392b78fb.png"
+  },
+  {
+    "revision": "c9b11593e18da546be9d174440cb837f",
+    "url": "/StarWarsCards/static/media/darthVader.c9b11593.png"
+  },
+  {
+    "revision": "19c19a4852f7515e8e5d0f83f844305b",
+    "url": "/StarWarsCards/static/media/han.19c19a48.jpg"
+  },
+  {
+    "revision": "4a8096f74e5f88c9b7d65a6d9b08a484",
+    "url": "/StarWarsCards/static/media/leia.4a8096f7.jpg"
+  },
+  {
+    "revision": "cd8f129d303257bff7bacad3339272b4",
+    "url": "/StarWarsCards/static/media/luke.cd8f129d.jpg"
+  },
+  {
+    "revision": "9ef5741a47930974768e67afaf6e3213",
+    "url": "/StarWarsCards/static/media/obiWan.9ef5741a.jpg"
+  },
+  {
+    "revision": "d441920a5525e1ab0e4139ffccdcba32",
+    "url": "/StarWarsCards/static/media/r2d2.d441920a.png"
+  },
+  {
+    "revision": "59a0353ee664321a4c7614c287573c98",
+    "url": "/StarWarsCards/static/media/space.59a0353e.jpg"
+  },
+  {
+    "revision": "b1d6f0732d4170617cd23c57ae3ff0d6",
+    "url": "/StarWarsCards/static/media/tarkin.b1d6f073.jpg"
+  }
+]);
